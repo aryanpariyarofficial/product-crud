e2a8558526d92154461328f02fffcb1a0699a1a0
aryanpariyarofficial <aryanpariyar8463@gmail.com>
2023-10-18 17:38:57 +0545

    api integrated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Home.css";
 import ricecooker from '../../assets/ricecooker.jpg';
+import axios from 'axios';
 const Home = () => {
-  return (
-    <>
-    <div className="flex">
-    <div className="card">
-        <img src={ricecooker} alt="Product Image" />
-        <div className="product-name">Product Name</div>
-        <div className="product-description">
-            This is a sample product description. You can replace this with your own description.
-        </div>
-    </div>
-    <div className="card">
-        <img src={ricecooker} alt="Product Image" />
-        <div className="product-name">Product Name</div>
-        <div className="product-description">
-            This is a sample product description. You can replace this with your own description.
-        </div>
-    </div>
-    <div className="card">
-        <img src={ricecooker} alt="Product Image" />
-        <div className="product-name">Product Name</div>
-        <div className="product-description">
-            This is a sample product description. You can replace this with your own description.
-        </div>
-    </div>
-    </div>
-    </>
-  )
+    const [products, setProducts] = useState([]);
+    const fetchProduct = async () => {
+        const response = await axios.get("https://652fbacf6c756603295d8ee6.mockapi.io/products");
+        console.log(response);
+        setProducts(response.data)
+    }
+    useEffect(() => {
+        fetchProduct();
+    }, []);
+    return (
+        <><div className="flex">
+            {
+                products.map((product) => {
+                    return (
+                        
+                            <div key={product.key} className="card">
+                                <img src={product.productImage} alt="Product Image" />
+                                <div className="product-name">{product.productName}</div>
+                                <div className="product-description">
+                                {product.productDescription}
+                                </div>
+                            </div>
+                       
+                    )
+                })
+            }
+             </div>
+
+        </>
+    )
 }
 
 export default Home
